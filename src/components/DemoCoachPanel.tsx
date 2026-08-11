3b98ec8754cf4fe04bc4bb609901b854d4587c04
Jay Choudhury <jyotirmoychoudhury@gmail.com>
2026-08-10 22:19:02 -0400

    Fix unused import

--- src/components/DemoCoachPanel.tsx
+++ src/components/DemoCoachPanel.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { useState } from "react-router-dom";
 import { mockClass } from '../data/mockClass'
 import { getStepNumber, normalizeMemberPath } from '../utils/navigation'
 import CoachDashboardContent from './CoachDashboardContent'
